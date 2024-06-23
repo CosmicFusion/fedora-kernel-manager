fd1fac0becea0b2d305b6e1f837b5006d524fd34
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-06-23 19:53:12 +0300

    fix policykit typos && and fix yum repo file name

--- data/scripts/kernel-cachyos-init.sh
+++ data/scripts/kernel-cachyos-init.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 YUM_CHANGED=false
+KERNEL_CACHYOS_REPO_FILE='/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos.repo'
+KERNEL_CACHYOS_ADDONS_REPO_FILE='/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons.repo'
 
-if [ ! -f /etc/yum.repos.d/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo ]
+if [ ! -f $KERNEL_CACHYOS_REPO_FILE ]
 then
-  wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
+  wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo -O $KERNEL_CACHYOS_REPO_FILE
   YUM_CHANGED=true
 fi
 
-if [ ! -f /etc/yum.repos.d/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo ]
+if [ ! -f $KERNEL_CACHYOS_ADDONS_REPO_FILE ]
 then
-  wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo
+  wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo -O $KERNEL_CACHYOS_ADDONS_REPO_FILE
   YUM_CHANGED=true
 fi
 
