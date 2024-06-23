be609d9e209e07f2215cb381e462098a8bdf7840
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-06-24 01:53:17 +0300

    Update change_scx.sh

--- data/scripts/change_scx.sh
+++ data/scripts/change_scx.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "scx_disabled" ]]; then
   systemctl stop scx & systemctl disable scx
 else
   set -e
-  sed -i 's/SCX_SCHEDULER=.*/SCX_SCHEDULER="$1"/' /etc/default/scx
+  sed -i "s/SCX_SCHEDULER=.*/SCX_SCHEDULER="$1"/" /etc/default/scx
   systemctl enable --now scx
   systemctl restart scx
 fi
